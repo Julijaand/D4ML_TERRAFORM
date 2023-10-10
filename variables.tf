4080f1f7fad709ba90bde23c3782e46905aba05a
github_julijaand <julijaand@yahoo.com>
2023-10-10 10:12:21 +0300

    variables added

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+    description = "The AWS region to create resource in"
+    default = "eu-central-1" # Frankfurt region
+}
+
+variable "key_name" {
+    description = "The name of the key pair"
+    default = "julias_key_pair"
+}
+
+variable "ami_id" {
+    description = "The ID of the AMI to use for the instance"
+    default = "ami-08a34e890d3f70022"
+}
+variable "instance_type" {
+    description = "The type of the instance"
+    default = "t2.micro"
+}
+
+variable "security_group" {
+    description = "The name of the security group"
+    default = "JuliasSecurityGroup"
+}
